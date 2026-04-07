6ea96c6b925dbc1eb8ac82025be7e1304a2ea906
wesley <20261ctb0100048@estudantes.ifpr.edu.br>
2026-04-07 18:42:36 +0000

    Aula 10 - 1004

---
+++ 1004.py
@@ -0,0 +1,21 @@
+'''
+Problema: beecrowd | 1004
+Data: 2026.04.07
+Estudante: Wesley Gustavo Dos Santos Rodrigues
+'''
+# Objetivo:
+
+# --- ANALISE (LIAC) ---
+# Entrada:A SOMA DE A * B
+# Processamento:Multiplicar A * B e armazenar em PROD
+# Saida:mostrar no formato exato "PROD = valor" (espaços ao redor =, sem mensagens extras)
+# Leitura Das Variaveis (obsever o enunciado - quantas entradas há?)
+
+A = int(input())
+B = int(input())
+
+# Qual operaçao o enunciado pede?
+PROD = A * B
+
+# Saída - observe o formato exato no enunciado
+print(f"PROD = {PROD}")
