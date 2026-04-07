85a1c253333b4338492570976daa331c3206a691
wesley <20261ctb0100048@estudantes.ifpr.edu.br>
2026-04-07 18:39:13 +0000

    Aula 10 - 1000

---
+++ 1000.py
@@ -0,0 +1,13 @@
+'''
+Problema: beecrowd | 1000
+data: 2026.04.07
+estudante: Wesley Gustavo Dos Santos Rodrigues
+'''
+# ojetivo: Fazer Aparecer a mensagem hello word
+
+#---ANÁLISE (LIAC) ---
+#Entrada: Este Problema Nao possui entrada
+#Processamento:
+#Saída:Fazer a Palavra Hello word aparecer
+
+print('Hello World!')
