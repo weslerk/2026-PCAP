dc021194b25f76c555ef26c96a7349ef463c268d
wesley <20261ctb0100048@estudantes.ifpr.edu.br>
2026-04-07 18:41:05 +0000

    Aula 10 - 1003

---
+++ 1003.py
@@ -0,0 +1,20 @@
+'''
+Problema: beecrowd | 1003
+Data: 2026.04.07
+Estudante: Wesley Gustavo Dos Santos Rodrigues
+'''
+# Objetivo: Fazer a soma de A e B 
+
+#--- ANÁLISE (LIAC) ---
+#Entrada: Dois numeros inteiros, cada um em uma linha
+#Processamento: somar A + B e armazenar em SOMA
+#Saída: mostrar no formato exato "SOMA = valor" (espaços ao redor =, sem mensagens extras)
+# Leitura Das Variaveis (obsever o enunciado - quantas entradas há?)
+A = int(input())
+B = int(input())
+
+#Calculo
+SOMA = A + B
+
+# Saida - observe o formato exato no enunciado
+print(f"SOMA = {SOMA}")
